c8a1ace087169c6ea42bc4e7c02684244c5ec93a
山南 <zhangpeichaook@163.com>
2018-11-14 15:01:28 +0800

    #wxApp.js

---
+++ client/wxApp.js
@@ -0,0 +1,27 @@
+
+function prependFunction(options, name, func) {
+    if (options[name]) {
+        var s = options[name];
+        options[name] = function (e) {
+            func.call(this, e, name), s.call(this, e);
+        };
+    } else {
+        options[name] = function (e) {
+            func.call(this, e, name);
+        };
+    }
+}
+
+let WxApp = function (options) {
+    prependFunction(options, 'onLaunch', function () { });
+    prependFunction(options, 'onShow', function (options) {
+        console.log(options);
+    });
+    prependFunction(options, 'onHide', function () { });
+    prependFunction(options, 'onError', function (error) { });
+    prependFunction(options, 'onPageNotFound', function (res) { });
+
+    App(options);
+};
+
+export default WxApp;
