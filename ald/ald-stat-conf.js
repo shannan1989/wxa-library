179638f988688bec875c5e9a8f66f4e636dd5be3
山南 <zhangpeichaook@163.com>
2022-05-27 08:49:55 +0800

    #update ald-stat to v7.5.0

--- ald/ald-stat-conf.js
+++ ald/ald-stat-conf.js
@@ -1,5 +1,5 @@
 import settings from '../settings.js';
 
 exports.app_key = settings.ald.appKey;
-exports.getLocation = settings.ald && settings.ald.getLocation;
 exports.plugin = false;
+exports.useCompliance = false; // 开启合规功能。默认不开启
