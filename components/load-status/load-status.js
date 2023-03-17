b1a66873f8236e521913c367faa50960b82e0823
山南 <zhangpeichaook@163.com>
2023-03-17 11:58:52 +0800

    #remove authorize

--- components/load-status/load-status.js
+++ components/load-status/load-status.js
@@ -14,26 +14,13 @@ Component({
                 else {
                     this.selectComponent("#loading").hide();
                 }
-
-                if (newVal == 'auth') {
-                    this.setData({ authing: true });
-                }
             }
         }
     },
-    data: {
-        authing: false
-    },
+    data: {},
     methods: {
         onRefresh() {
             this.triggerEvent('Refresh', {}, {});
-        },
-        onAuthOK() {
-            let data = { authing: false };
-            if (this.data.status == 'auth') {
-                data.status = 'loading';
-            }
-            this.setData(data);
         }
     }
 });
